53e4480460e9b5d01000deb81d8a74262f59ec0c
Jose G Martinez <jgermanmm@gmail.com>
2024-11-04 14:14:03 -0300

    Agregar configuración básica del servidor y rutas de autenticación

---
+++ src/presentation/auth/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+
+export class AuthRoutes {
+
+    static get routes(): Router {
+        const router = Router();
+
+        router.post('/login', (req, res) => {
+            res.send('login');
+        });
+        router.post('/register', (req, res) => {
+            res.send('register');
+        });
+
+        return router;
+    }
+}
